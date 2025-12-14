17f6d003a0199944036c1e4b4f6a120a1a2d1cfb
Khushpansare <hkhushalpansare@gmail.com>
2025-12-14 18:16:07 +0530

    smooth-scroll on all over body

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React, { useRef } from "react";
 import Navbar from "./dashboard/Navbar";
-import Component_Wrapper from "./wrapper/Component_Wrapper";
 import Main_Page from "./component/Main_Page";
 
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smooth: true,
+      smoothTouch: false,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <>
       <Navbar />
